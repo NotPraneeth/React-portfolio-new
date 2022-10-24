6a2afd00a1086e08ef2fce4dc1b59890ba10d439
NotPraneeth <rsphaha@gmail.com>
2022-10-25 01:35:17 +0530

    removed locomotive scroll temporaily due to bug

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -4,9 +4,9 @@ import skillsbg from './assets/ring.mp4'
 function Skills() {
   return (
     <div className='Skills-container'>
-      <div className='Skills-bg-container'>
+      
         <video src={skillsbg} autoPlay loop muted id='skillsbg' type='video/mp4'/>
-      </div>
+     
       
     </div>
   )
