ed1851c08423d485d8a5d126162f7dada77366e7
NotPraneeth <rsphaha@gmail.com>
2022-10-25 02:34:41 +0530

    worked on the skills page

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import './skills.css'
 import skillsbg from './assets/ring.mp4'
+import Marquee from "react-fast-marquee";
 function Skills() {
   return (
     <div className='Skills-container'>
       
         <video src={skillsbg} autoPlay loop muted id='skillsbg' type='video/mp4'/>
+        <div className='skills-content'>
+          <div className='skills1'>WHAT I AM GOOD AT</div>
+          <Marquee  direction='right' speed={80} gradient={false} className='Skills-marquee1'>
+            GIT. BOOTSRAP. SASS. BLENDER. TAILWIND
+          </Marquee>
+          <Marquee  gradient={false} speed={80} className='Skills-marquee2'>
+            REACT. SASS. JAVASCRIPT. HTML. CSS. 
+          </Marquee>
+          <div className='skills2'>LET'S TALK WORK</div>
+        </div>
      
       
     </div>
