2d1eb4ad9824d47ac041b92541dcef6e8905ad26
NotPraneeth <rsphaha@gmail.com>
2022-10-28 10:16:30 +0530

    changed skills bg and improved QOL

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './skills.css'
-import skillsbg from './assets/ring.mp4'
+import skillsbg from './assets/nightlight.mp4'
 import Marquee from "react-fast-marquee";
 function Skills() {
   return (
@@ -9,11 +9,11 @@ function Skills() {
         <video src={skillsbg} autoPlay loop muted id='skillsbg' type='video/mp4'/>
         <div className='skills-content'>
           <div className='skills1'>WHAT I AM GOOD AT</div>
-          <Marquee  direction='right' speed={80} gradient={false} className='Skills-marquee1'>
-            GIT. BOOTSTRAP. SASS. BLENDER. TAILWIND
+          <Marquee  direction='right' speed={140} gradient={false} className='Skills-marquee1'>
+            GIT BOOTSTRAP SASS TAILWIND
           </Marquee>
-          <Marquee  gradient={false} speed={80} className='Skills-marquee2'>
-            REACT. SASS. JAVASCRIPT. HTML. TAILWIND.  
+          <Marquee  gradient={false} speed={140} className='Skills-marquee2'>
+            REACT BLENDER JAVASCRIPT HTML 
           </Marquee>
           <div className='skills2'>LET'S TALK WORK</div>
         </div>
