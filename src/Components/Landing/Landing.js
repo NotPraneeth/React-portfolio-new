50f0a5f888cdedfe4a032cc1bcd44cf968079115
NotPraneeth <rsphaha@gmail.com>
2022-11-03 22:13:12 +0530

    cleaned up the view and made it look better

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -11,18 +11,18 @@ function Landing() {
   const transition = {duration:0.6,ease:[0.6,0.1,-0.05]}
   return (
     <>
-        <motion.div initial={{opacity:0 }} animate={{opacity:1}} transition={{transition}}className='Landing-container'>
+    <motion.div initial={{opacity:0 }} animate={{opacity:1}} transition={{transition}}className='Landing-container'>
     <div>
         <motion.div className='Landing-hero-content'>
           <span className='landing-text'>
             <div className='hello-container'>
-              <motion.span initial={{y:"100%"}} animate={{y:"0"}} transition={{duration:1,ease:'easeOut'}} className='hello-landing'>hello</motion.span>
+              <motion.span initial={{y:"100%"}} whileInView={{y:"0"}} transition={{duration:1,ease:'easeOut'}} className='hello-landing'>hello</motion.span>
               <div className='hello-text-container'>
-              <motion.div initial={{y:"300%"}} animate={{y:"100%"}} transition={{duration:1,ease:'easeOut',delay:0.2}} className='hello-text'> your project is cool 😎 <br></br> but let's make it cooler🎉</motion.div>
+              <motion.div initial={{y:"300%"}} whileInView={{y:"100%"}} transition={{duration:1,ease:'easeOut',delay:0.2}} className='hello-text'> your project is cool 😎 <br></br> but let's make it cooler🎉</motion.div>
               </div>
             </div>
             <div className='world-container'>
-            <motion.span initial={{y:"100%"}} animate={{y:"0"}} transition={{duration:1,ease:'easeOut',delay:0.2}} className='world-landing'>World</motion.span>
+            <motion.span initial={{y:"100%"}} whileInView={{y:"0"}} transition={{duration:1,ease:'easeOut',delay:0.2}} className='world-landing'>World</motion.span>
             </div>
             </span>
             </motion.div>
