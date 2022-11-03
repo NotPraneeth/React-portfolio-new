dd8adb30bfef31777bb210ce8ffb0b3808c3575f
NotPraneeth <rsphaha@gmail.com>
2022-11-03 23:13:47 +0530

    added resume button

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -12,10 +12,10 @@ function Navbar() {
         
         <div className='socials-nav'>
                <ul className='list-nav'>
-                  <motion.li initial={{y:"-100%" }} whileInView={{y:0}} transition={{duration:0.75,ease:"easeOut"}}><Link to='/'>HOME</Link></motion.li>
-                  <motion.li initial={{y:"-100%" }} whileInView={{y:0}} transition={{duration:1.00,ease:"easeOut"}}><Link to = '/about'>ABOUT</Link></motion.li>
-                  <motion.li initial={{y:"-100%" }} whileInView={{y:0}} transition={{duration:1.25,ease:"easeOut"}} ><Link to = '/skills'>SKILLS</Link></motion.li>
-                  <motion.li initial={{y:"-100%" }} whileInView={{y:0}} transition={{duration:1.5,ease:"easeOut"}} ><Link to = '/contact'>CONTACT</Link></motion.li>
+                  <motion.li initial={{y:"-100%" }} whileInView={{y:0}} transition={{duration:0.75,ease:"easeOut"}}><Link to = '/about'>ABOUT</Link></motion.li>
+                  <motion.li initial={{y:"-100%" }} whileInView={{y:0}} transition={{duration:1.00,ease:"easeOut"}} ><Link to = '/skills'>SKILLS</Link></motion.li>
+                  <motion.li initial={{y:"-100%" }} whileInView={{y:0}} transition={{duration:1.25,ease:"easeOut"}} ><Link to = '/contact'>CONTACT</Link></motion.li>
+                  <motion.button initial={{y:"-100%" }} whileInView={{y:0}} transition={{duration:1.5,ease:"easeOut"}} className='download-btn'><Link to='/files/resume.pdf' className='btn-link' target='_blank' download>DONWLOAD RESUME</Link></motion.button>
 
               </ul>
             </div>
