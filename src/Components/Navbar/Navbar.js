58b9cc3f2487e7d05c249a29f307b96fe7c555c9
NotPraneeth <rsphaha@gmail.com>
2022-11-04 00:38:59 +0530

    nav-button sliding effect

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -15,8 +15,8 @@ function Navbar() {
                   <motion.li initial={{y:"-100%" }} whileInView={{y:0}} transition={{duration:0.75 ,ease:"easeOut"}}><Link to = '/about'>ABOUT</Link></motion.li>
                   <motion.li initial={{y:"-100%" }} whileInView={{y:0}} transition={{duration:1.00,ease:"easeOut"}} ><Link to = '/skills'>SKILLS</Link></motion.li>
                   <motion.li initial={{y:"-100%" }} whileInView={{y:0}} transition={{duration:1.25,ease:"easeOut"}} ><Link to = '/contact'>CONTACT</Link></motion.li>
-                  <motion.div initial={{width:"0%" }} whileInView={{width:"100%"}} transition={{duration:1.25,ease:"easeOut"}} className= 'button-container'>
-                  <motion.button initial={{y:"-100%" }} whileInView={{y:0}} transition={{duration:1.5,ease:"easeOut"}} className='download-btn'><Link to='/files/resume.pdf' className='btn-link' target='_blank' download>DONWLOAD RESUME</Link></motion.button>
+                  <motion.div initial={{width:"0%" }} whileInView={{width:"100%"}} transition={{duration:4.25,ease:[0.6,1,-0.07,-0.02]}} className= 'button-container'>
+                  <motion.button className='download-btn'><Link to='/files/resume.pdf' className='btn-link' target='_blank' download>DONWLOAD RESUME</Link></motion.button>
                   </motion.div>
 
               </ul>
