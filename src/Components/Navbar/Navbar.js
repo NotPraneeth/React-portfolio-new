918df2227cd2dd8a8e094c0987079d0b965ab4d4
NotPraneeth <rsphaha@gmail.com>
2022-11-03 23:21:19 +0530

    worked on navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -12,10 +12,12 @@ function Navbar() {
         
         <div className='socials-nav'>
                <ul className='list-nav'>
-                  <motion.li initial={{y:"-100%" }} whileInView={{y:0}} transition={{duration:0.75,ease:"easeOut"}}><Link to = '/about'>ABOUT</Link></motion.li>
+                  <motion.li initial={{y:"-100%" }} whileInView={{y:0}} transition={{duration:0.75 ,ease:"easeOut"}}><Link to = '/about'>ABOUT</Link></motion.li>
                   <motion.li initial={{y:"-100%" }} whileInView={{y:0}} transition={{duration:1.00,ease:"easeOut"}} ><Link to = '/skills'>SKILLS</Link></motion.li>
                   <motion.li initial={{y:"-100%" }} whileInView={{y:0}} transition={{duration:1.25,ease:"easeOut"}} ><Link to = '/contact'>CONTACT</Link></motion.li>
+                  <motion.div initial={{width:"0%" }} whileInView={{width:"100%"}} transition={{duration:1.25,ease:"easeOut"}} className= 'button-container'>
                   <motion.button initial={{y:"-100%" }} whileInView={{y:0}} transition={{duration:1.5,ease:"easeOut"}} className='download-btn'><Link to='/files/resume.pdf' className='btn-link' target='_blank' download>DONWLOAD RESUME</Link></motion.button>
+                  </motion.div>
 
               </ul>
             </div>
