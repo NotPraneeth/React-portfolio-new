b2aa9c58dde0c315e1a4b257afb3ef096b38f4b9
NotPraneeth <rsphaha@gmail.com>
2022-11-04 20:02:52 +0530

    fixed download button returning file not found

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './navbar.css'
 import { motion } from "framer-motion"
+import resume from './assets/Resume.pdf'
 function Navbar() {
   return (
     <div className='navbar-container'>
@@ -16,7 +17,7 @@ function Navbar() {
                   <motion.li initial={{y:"-100%" }} whileInView={{y:0}} transition={{duration:1.00,ease:"easeOut"}} ><Link to = '/skills'>SKILLS</Link></motion.li>
                   <motion.li initial={{y:"-100%" }} whileInView={{y:0}} transition={{duration:1.25,ease:"easeOut"}} ><Link to = '/contact'>CONTACT</Link></motion.li>
                   <motion.div initial={{y:"-100%" }} whileInView={{y:0}} transition={{duration:1.25,ease:"easeOut"}} className= 'button-container'>
-                  <motion.button className='download-btn'><Link to='/files/resume.pdf' className='btn-link' target='_blank' download>DONWLOAD RESUME</Link></motion.button>
+                  <motion.button className='download-btn'><Link to={resume} className='btn-link' target='_blank' download>DONWLOAD RESUME</Link></motion.button>
                   </motion.div>
 
               </ul>
